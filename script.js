dd82289382502f9750ef50b16ee2b51826ce2d57
cheeky sim <48182295+cheekysim@users.noreply.github.com>
2022-05-26 15:30:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    const ads = ["images/advert1.jpg", "images/advert2.jpg", "images/advert3.jpg"];
+    const ads = ["images/adverts/advert1.jpg", "images/adverts/advert2.jpg", "images/adverts/advert3.jpg"];
     const random = Math.floor(Math.random() * ads.length);
     var ad = (random, ads[random]);
     document.getElementById("advert").src=ad;
